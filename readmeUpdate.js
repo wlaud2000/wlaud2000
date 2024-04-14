a7109b03c9c34eab1f502e8132f2a5202a368691
Jimyeong-Kim <86175262+wlaud2000@users.noreply.github.com>
2024-04-14 10:17:05 +0900

    Update readmeUpdate.js

--- readmeUpdate.js
+++ readmeUpdate.js
@@ -14,7 +14,6 @@ let text = `# Hi I'm Jimyeong! 👋
  <a href="https://hits.seeyoufarm.com"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fwlaud2000&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false"/></a></a>
 </p>
 
-## 이런 환경에 익숙해요✍🏼
 
 ## 📚Skills
 <!-- https://shields.io/ -->
