3aac770cbaa67f229029d90c637088cf8e90eb79
Jimyeong-Kim <86175262+wlaud2000@users.noreply.github.com>
2024-10-03 21:18:37 +0900

    Update readmeUpdate.js

--- readmeUpdate.js
+++ readmeUpdate.js
@@ -10,9 +10,6 @@ let text = `# Hi I'm Jimyeong! 👋
 [![Anurag's GitHub stats](https://github-readme-stats.vercel.app/api?username=wlaud2000)](https://github.com/anuraghazra/github-readme-stats)
 [![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=wlaud2000&layout=compact&hide=r,jupyter%20notebook,c%23&exclude_repo=roharui.github.io)](https://github.com/anuraghazra/github-readme-stats)
 
-<p align="center">
- <a href="https://hits.seeyoufarm.com"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fwlaud2000&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false"/></a></a>
-</p>
 
 
 ## 📚Skills
